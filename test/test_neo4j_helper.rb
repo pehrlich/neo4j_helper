0d8f75cf00da562b6c781f759cef749917b14b53
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-01-25 11:51:05 -0500

    Initial commit to neo4j_helper.

---
+++ test/test_neo4j_helper.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestNeo4jHelper < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end

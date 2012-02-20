f3d7796c4e9e284b8755e7b133eefc293999db08
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-02-20 17:57:04 -0500

    add rels_to, rels_from, relate, and ensure_relation

--- lib/neo4j_helper/model.rb
+++ lib/neo4j_helper/model.rb
@@ -22,9 +22,48 @@ module Neo4j
         resource.update_attributes(attributes)
       end
 
+      def ensure_relation(type, options = {}, props = {})
+        # options: to and from
+        if end_node = options[:to]
+          rels = rels_to(end_node, type: type)
+        elsif start_node = options[:from]
+          rels = rels_from(start_node, type: type)
+        end
 
-      def rels_to(node)
-        self.rels.to_other(node)
+        if rels.present?
+          rel = rels.first
+          #rel.attributes = props
+          rel.update_attributes props
+          rel
+        else
+          relate(type, options, props)
+        end
+
+      end
+
+
+      def relate(type, options = {}, props = {})
+        if end_node = options[:to]
+          start_node = self
+        elsif start_node = options[:from]
+          end_node = self
+        else
+          raise ':to or :from is required'
+        end
+        Neo4j::Rails::Relationship.create(type, start_node, end_node, props)
+      end
+
+
+      def rels_to(end_node, options = {})
+        if type = options[:type]
+          rels(type).to_other(end_node)
+        else
+          self.rels.to_other(end_node)
+        end
+      end
+
+      def rels_from(start_node, options = {})
+        start_node.rels_to(self, options)
       end
 
       def rel_to(node)
